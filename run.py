dd5c081194f7892b8b07daebefa7339f2bf6e081
EllisWitten <e.witten.2001@gmail.com>
2022-05-09 08:30:15 +0000

    add a fucntion to check to see if the name matches an other data entries to avoid duplicates

--- run.py
+++ run.py
@@ -22,16 +22,18 @@ def get_student_grades(data):
     """
     while True:
         print('Welcome to School Prediciton.')
-        print('Enter your name and grades to find out what college to aplly for.\n')
+        print('Enter your name and grades to find out what college to apply for.\n')
         student_name = input('Enter your name here (example:John Smith):\n')
         print('Your grades should be entered as so; English, Maths, Science, Option1, Option2, Option3\n')
         student_grades_inp = input('Please enter your grades here:')
         student_grades = student_grades_inp.split(',')
         student_info = [student_name,student_grades]
         
-        if validate_name(student_info) and validate_grades(student_info):
-            print('Data is valid')
+        if validate_name(student_info) and validate_grades(student_info) and check_for_duplicate(student_info):
+            print('data is valid\n')
             break
+        elif validate_name(student_info) and validate_grades(student_info) and (check_for_duplicate(student_info) is not True):
+            print('Data has already been entered\n')
     return (student_info)
 
 def validate_name(data):
@@ -39,12 +41,10 @@ def validate_name(data):
     Check the name entered to make sure that only etters and spaces are entered
     """
     name = data[0]
-    print(f'this is your name:{name}')
     if all(letter.isalpha() or letter.isspace() for letter in name):
-        print(f'{name} is a valid name')
         return True
     else:
-        print(f'{name} is invalid, must only contain letters')
+        print(f'{name} is invalid, must only contain letters\n')
         return False
 
 def validate_grades(data):
@@ -53,21 +53,35 @@ def validate_grades(data):
     """
     grades = data[1]
     if len(grades) == 6:
-        print('Grades are valid')
+        print('Grades are valid\n')
         return True
     else:
-        print('Grades are invlaid, please enter 6 letters seperated by commas')
+        print('Grades are invlaid, please enter 6 letters seperated by commas\n')
 
 def update_worksheet(data, worksheet):
-    student_info = [data[0].capitalize()]
+    """
+    Update the given worksheet with the data in the correct format
+    """
+    student_info = [data[0].title()]
     for x in data[1]:
         student_info.append(x.capitalize())
-    print(student_info)
     print(f'Updating {worksheet.capitalize()} worksheet...\n')
     spec_worksheet = SHEET.worksheet(worksheet)
     spec_worksheet.append_row(student_info)
     print(f'{worksheet.capitalize()} worksheet updated successfully.\n')
 
+def check_for_duplicate(data):
+    """
+    Check the database to see if their name has already been entered to avoid duplicates.
+    """
+    name = data[0].title()
+    all_names = SHEET.worksheet('grades').col_values(1)
+    if name in all_names:
+        print(f'{name} has already been entered\n')
+        return False
+    else:
+        print('name has not been entered\n')
+        return True
 def main():
     student = get_student_grades(data)
     update_worksheet(student, 'grades')
